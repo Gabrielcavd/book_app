50f78a66165b22b0932e598f8f86985fc7277c6b
Gabriel Cavalcante <80011482+Gabrielcavd@users.noreply.github.com>
2026-03-05 14:45:06 -0300

    Initial Commit

---
+++ book_app/book_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  book_appApp.swift
+//  book_app
+//
+//  Created by Gabriel Cavalcante on 05/03/26.
+//
+
+import SwiftUI
+
+@main
+struct book_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
