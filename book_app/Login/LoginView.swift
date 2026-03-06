5b13bdaf91c735b9501e32da8574c1747e381512
Gabriel Cavalcante <80011482+Gabrielcavd@users.noreply.github.com>
2026-03-06 10:56:14 -0300

    add navigation

--- book_app/Login/LoginView.swift
+++ book_app/Login/LoginView.swift
@@ -11,55 +11,59 @@ struct LoginView: View {
     @State private var email: String = ""
     @State private var password: String = ""
     var body: some View {
-        ScrollView(.vertical){
-            VStack(alignment: .leading) {
-                Image("logo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 113, height: 53)
+        NavigationStack {
+            ScrollView(.vertical) {
+                VStack(alignment: .leading) {
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 113, height: 53)
 
-                Text("Faça login na\n sua conta.")
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
+                    Text("Faça login na\n sua conta.")
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
 
-                TextFieldLabel(text: $email, label: "Email", titleKey: "Insira seu email")
-                    .padding(.vertical, 28)
+                    TextFieldLabel(text: $email, label: "Email", titleKey: "Insira seu email")
+                        .padding(.vertical, 28)
 
-                TextFieldLabel(text: $password, label: "Senha", titleKey: "Insira sua senha")
+                    TextFieldLabel(text: $password, label: "Senha", titleKey: "Insira sua senha")
 
-                Button("Esqueceu sua senha?") {
+                    Button("Esqueceu sua senha?") {
+                    }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+
+                    NavigationLink(destination: PreferencesView(), label: {
+                        PrimaryButton(title: "Entrar") {
+                        }
+                    })
+                    .padding(EdgeInsets(top: 30, leading: 0, bottom: 20, trailing: 0))
+
+                    HStack {
+                        line
+                        Text("Ou entre com")
+                            .foregroundStyle(.gray)
+                        line
+                    }
+                    .padding(.bottom, 28)
+
+                    HStack(alignment: .center) {
+                        LoginOptionIcon(icon: "google")
+                            .padding(.trailing, 16)
+                        LoginOptionIcon(icon: "apple")
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.bottom, 28)
+
+                    Button("Ainda não tem uma conta?") {
+                    }
+                    .frame(maxWidth: .infinity)
                 }
-                .frame(maxWidth: .infinity, alignment: .trailing)
-
-                PrimaryButton(title: "Entrar") {
-                }
-                .padding(EdgeInsets(top: 30, leading: 0, bottom: 20, trailing: 0))
-
-                HStack {
-                    line
-                    Text("Ou entre com")
-                        .foregroundStyle(.gray)
-                    line
-                }
-                .padding(.bottom, 28)
-
-                HStack(alignment: .center) {
-                    LoginOptionIcon(icon: "google")
-                        .padding(.trailing, 16)
-                    LoginOptionIcon(icon: "apple")
-                }
-                .frame(maxWidth: .infinity)
-                .padding(.bottom, 28)
-                
-                Button("Ainda não tem uma conta?") {
-                }
-                .frame(maxWidth: .infinity)
             }
         }
         .padding()
     }
 
-    var line: some View {
+    private var line: some View {
         VStack { Divider().background(.gray) }.padding(10)
     }
 }
---
+++ book_app/Preferences/PreferencesView.swift
@@ -0,0 +1,18 @@
+//
+//  PreferencesView.swift
+//  book_app
+//
+//  Created by Gabriel Cavalcante on 06/03/26.
+//
+
+import SwiftUI
+
+struct PreferencesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PreferencesView()
+}
